ebee5f4205107e4300ae024c816f6240a1175613
Turgon37 <pgindraud+github@gmail.com>
2015-08-02 01:45:38 +0200

    Reparse code in python3
    Convert base code into python3
    Improve login procedure
    Add Logout function
    Put lttle print_r function to format output of python array

--- livebox/livebox.py
+++ livebox/livebox.py
@@ -1,162 +1,247 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
 
-import urllib
-import urllib2
+# This file is a part of Livebox_tools
+#
+# Copyright (c) 2015 Pierre GINDRAUD
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""Livebox Tools
+
+This file provide some tools to use new Livebox 3 Firmware (Soft At Home) API
+"""
+
+import urllib.parse
+import http.client
+from http.client import HTTPException
 import json
-
 import getpass
-
-# Livebox.login()              Authentification session utilisateur
-# Livebox.logout()             Fermeture session
-# Livebox.listTrunks()         Information téléphonie IP
-# Livebox.getIPTVStatus()      Etat IP TV
-# Livebox.getDSLStats()        Stats DSL (Erreurs)
-# Livebox.getMIBs()            Information lien dsl
-# Livebox.getWifiMIBs()        Information Wifi
-# Livebox.getWANStatus()       Etat de la connexion WAN
-# Livebox.reboot()             Redémarrage la livebox
-# Livebox.setwifi(mode)        Active/désactive le Wifi
-# Livebox.filtreMAC(flag)      Active/Désactive filtrage MAC address
-# Livebox.getWificomStatus()   Information Wifi public Orange
-
 
 
 class Livebox:
+  """This is the main API requester class
+  """
 
-	def __init__(self,url_prefix='https://livebox'):
-		self._url_prefix = url_prefix
+  PASSWORD = ""
+  USERNAME = "admin"
+  URL_P_AUTH = "/authenticate?username={username}&password={password}"
+  URL_P_LOGOUT = "/logout"
+
+  def __init__(self, protocol='http', host='livebox'):
+    """ Build a Livebox API client
+
+    @param[in] proto : the http protocol to use (http, https)
+    @parma[in] host : the domain name of the target livebox host
+    """
+    self.__protocol = protocol
+    self.__host = host
+    self.__port = http.client.HTTP_PORT
+    self.base_url = "{:s}://{:s}".format(protocol, host)
+
+    self.__connection = None
+    self.__cookies = ""
+    self.__contextID = ""
+    self.__is_connected = False
+
+# AUTHENTIFICATION SECTION
+
+  def login(self, password=None):
+    """Authenticate to Livebox host
+
+    This function try to perform a full Authenticate procedure
+    to the livebox host
+
+    @param[in] password : is provided the password to use as credential
+          if not provided use PASSWORD class attribute if available
+          if not of them are available the password is asked to user
+    """
+    if password is None:
+      if Livebox.PASSWORD:
+        password = Livebox.PASSWORD
+      else:
+        password = getpass.getpass()
+
+    values = {'username': 'admin',
+              'password': password}
+
+    response = self.__query(Livebox.URL_P_AUTH.format(**values),
+                            urllib.parse.urlencode(values).encode('utf8'))
+    if response is not None and response.status == http.client.OK:
+      datas = json.loads(response.read().decode('utf8'))
+      # save connection parameters
+      self.__contextID = datas['data']['contextID']
+      self.__cookies_code = response.info()['Set-Cookie'].split('/')[0]
+      self.__cookies = (
+          response.info()['Set-Cookie'].split(';')[0] + "; " +
+          self.__cookies_code + "/login=" + values['username'] + "; " +
+          self.__cookies_code + "/context=" + self.__contextID)
+
+      self.__is_connected = True
+
+  def logout(self):
+    """Logout from Livebox host
+
+    Make a logout query to livebox
+    """
+    if not self.__is_connected:
+      return
+    response = self._queryAuth(Livebox.URL_P_LOGOUT)
+    self.__is_connected = False
+
+# QUERY SECTION
+  def __url(self, query):
+    """Compute the full URL from query string
+
+    @param[in] query : the query part of the url
+    @return [string] the full url with proto+host+query
+    """
+    return self.base_url + query
+
+  def __query(self, query, params='', headers=dict(),
+              content_type='application/json'):
+    """Make a basic HTTP POST query to host
+
+    @param[in] query : the query string (part after / in URL)
+    @param[in] params OPTIONNAL : the data (body) of the query
+    @param[in] headers OPTIONNAL : the additionnal headers to put in the query
+    @param[in] content_type OPTIONNAL : type of content to request to host
+    @return HTTPResponse if query success, None otherwise
+    """
+    if isinstance(params, str):
+      data = params.encode('utf8')
+    else:
+      data = params
+
+    if self.__connection is None:
+      self.__connection = http.client.HTTPConnection(self.__host,
+                                                     self.__port)
+
+    headers['Connection'] = 'keep-alive'
+    headers['User-Agent'] = 'Python Livebox Tools'
+    if 'Content-Type' not in headers:
+      headers['Content-Type'] = content_type
+
+    try:
+      self.__connection.request('POST', query, data, headers)
+      response = self.__connection.getresponse()
+    except HTTPError as e:
+      print(e)
+      return None
+    return response
+
+  def _queryUnauth(self, query, params=''):
+    """Execute a simple query without any specific headers
+
+    @param[in] query : the query string (part after / in URL)
+    @param[in] params OPTIONNAL : the data (body) of the query
+    @return A dict which contains JSON response datas or empty str
+    """
+    r_data = self.__query(query, params).read()
+    if r_data:
+      return json.loads(r_data.decode('utf8'))
+    else:
+      return ""
+
+  def _queryAuth(self, query, params='', headers=dict()):
+    """Execute a query with authentication specific headers
+
+    @param[in] query : the query string (part after / in URL)
+    @param[in] params OPTIONNAL : the data (body) of the query
+    @param[in] headers OPTIONNAL : the additionnal headers to put in the query
+    @return A dict which contains JSON response datas or empty str
+    """
+    headers['Cookie'] = self.__cookies
+    headers['X-Context'] = self.__contextID
+    headers['X-Sah-Request-Type'] = 'idle'
+    headers['X-Requested-With'] = 'XMLHttpRequest'
+    headers['X-Prototype-Version'] = '1.7'
+    headers['Content-Type'] = "application/x-sah-ws-1-call+json; charset=UTF-8"
+    headers['DNT'] = '1'
+
+    response = self.__query(query, params, headers)
+    r_data = response.read()
+    r_header = dict(response.getheaders())
+    if r_data and r_header['Content-Type'].split(';')[0] == 'application/json':
+      return json.loads(r_data.decode('utf8'))
+    else:
+      return ""
+
+  def _sysbus(self, quest, param='{"parameters":{}}', auth=False):
+    """Execute a SYSBUS call on the host
+
+    @param[in] quest : the query object in string format
+    @param[in] params OPTIONNAL : the data (body) of the query
+    @param[in] auth OPTIONNAL : a boolean that indicated if the query must be
+              executed as authenticated
+    """
+    if auth:
+      return self._queryAuth("/sysbus/{:s}".format(quest), param)
+    else:
+      return self._queryUnauth("/sysbus/{:s}".format(quest), param)
+
+  def require_auth(func):
+    """ Apply this decorator to all method that require to be authentified
+    This decorator put a basic verification to check that given method is call
+    in a authenticated environnement
+    """
+    def check_auth(self):
+      """Check if the current connection s authenticated on the host or not
+
+      If not authenticated return empty string
+      """
+      if self.__is_connected:
+        return func(self)
+      else:
+        return ""
+    return check_auth
 
 
 
-	def login(self):
-	
-		password=getpass.getpass()
-	
-		url_login = "%s/authenticate?username=admin&password=%s" % (self._url_prefix,password)
-	
-		values = {'username' : 'admin',
-	          	'password' : password }
-	
-		data = urllib.urlencode(values)
-		req = urllib2.Request(url_login, data)
-		response = urllib2.urlopen(req)
-		the_page = response.read()
-		
-		datas = json.loads(the_page)
-		
-		self._cookies=response.info()['Set-Cookie'].split(';')[0]
-		self._contextID = datas['data']['contextID']
-	
+def print_r(content, align=''):
+  """ Show a JSON array into a more visual form
 
-	def _sysbus(self,question):
-		return self._request("%s/sysbus/%s" % (self._url_prefix,question)
-			, '{"parameters":{}}' )
-
-	def _ws(self, params):
-		return self._request("%s/ws" % (self._url_prefix)
-			, params , 'application/x-sah-event-2-call+json; charset=UTF-8')
-
-	def _request(self,url, params, content_type='application/json'):
-	
-
-		print '#--------------------------------------------------------------------'
-		print "%s ( %s )" % (url,params)
-		print '#--------------------------------------------------------------------'
-
-			
-		req = urllib2.Request(url, params)
-		req.add_header('Cookie', self._cookies)
-		req.add_header('X-Context', self._contextID)
-		req.add_header('X-Sah-Request-Type', 'idle')
-		req.add_header('Content-Type', content_type)
-		req.add_header('X-Requested-With', 'XMLHttpRequest')
-	
-		response = urllib2.urlopen(req)
-		the_page = response.read()
-	
-		return json.loads(the_page)
+  Use this as debug
+  """
+  if isinstance(content, dict):
+    for key in content:
+      head = align + str(key) + " => "
+      print(head)
+      print_r(content[key], ' '*len(head))
+  elif isinstance(content, list):
+    for val in content:
+      print_r(val, ' '*len(align))
+  else:
+    value = str(content)
+    if value:
+      print(align + value)
 
 
-
-	def logout(self):
-        	urllib2.urlopen("%s/logout" % (self._url_prefix))
-		return ''
-
-	def list_trunks(self):
-		'''Information téléphonie IP'''
-		return self._sysbus('VoiceService/VoiceApplication:listTrunks')
-
-	def ip_tv_status(self):
-	      	'''Etat IP TV'''
-		return self._sysbus('NMC/OrangeTV:getIPTVStatus')
-
-	def dsl_stats(self):
-	        '''Stats DSL (Erreurs)'''
-		pass
-
-	def mibs(self):
-		'''Information lien dsl'''
-		pass
-
-	def wifi_com_status(self):
-		'''Information Wifi public Orange)'''
-		return self._sysbus('Wificom:getStatus')
-
-	def wiwi_mibs(self):
-		'''Information Wifi'''
-		pass
-
-	def wan_status(self):
-		'''Etat de la connexion WAN'''
-		return self._sysbus('NMC:getWANStatus')
-
-	def reboot(self):
-		'''Redémarrage la livebox'''
-		pass
-
-	def wifi(self,mode):
-		'''Active/désactive le Wifi'''
-		pass
-
-	def filtre_mac(self,flag):
-		'''Active/Désactive filtrage MAC address'''
-		pass
-
-	def connectivity_enable_notifications(self):
-		return self._sysbus('sah/hgw/models/Connectivity:enableNotifications')
-
-
-
-	def ws_channel_id(self):
-		j = self._ws('{"events":[{"handler":"sah.hgw.models"}]}')
-		return j['channelid']
-
-	def ws_get_devices(self, channel_id):
-		j = self._ws('{"events":[{"handler":"sah.hgw.models"}],"channelid":%s}' % channel_id)
-		return j
 
 
 if __name__ == '__main__':
-	lb = Livebox()
-	lb.login()
-	#lb.sysbus('DeviceInfo?_restDepth=-1', c, C)
-	#print lb._sysbus('')
-	print lb.wan_status()
-	print lb.list_trunks()
-	print lb.ip_tv_status()
-	print lb.dsl_stats()
-	print lb.mibs()
-	print lb.wifi_com_status()
-	print lb.wiwi_mibs()
-	# print lb.wifi(mode)
-	# print lb.filtre_mac(flag)
-	# print lb.reboot()
-	lb.connectivity_enable_notifications()
-	print lb.ws_get_devices(lb.ws_channel_id())
+  lb = Livebox()
+  lb.login()
 
-	print lb._sysbus('')
-	print lb.logout()
+
+  lb.logout()
+
 
 
 

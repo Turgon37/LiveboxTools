2d23c58b5e6f9bb1c3776acc69c5d30be01ff119
Turgon37 <pgindraud+github@gmail.com>
2015-08-02 13:16:28 +0200

    Update core API
    Add action decorator for LIVEBOX action call
    Add 'method' parameter to allow selection of GET and POST separatly of the query function

--- livebox/livebox.py
+++ livebox/livebox.py
@@ -30,7 +30,7 @@ This file provide some tools to use new Livebox 3 Firmware (Soft At Home) API
 
 import urllib.parse
 import http.client
-from http.client import HTTPException
+from http.client import *
 import json
 import getpass
 
@@ -115,13 +115,15 @@ class Livebox:
     return self.base_url + query
 
   def __query(self, query, params='', headers=dict(),
-              content_type='application/json'):
-    """Make a basic HTTP POST query to host
+              content_type='application/json',
+              method='POST'):
+    """Make a basic HTTP query to host
 
     @param[in] query : the query string (part after / in URL)
     @param[in] params OPTIONNAL : the data (body) of the query
     @param[in] headers OPTIONNAL : the additionnal headers to put in the query
     @param[in] content_type OPTIONNAL : type of content to request to host
+    @param[in] method OPTIONNAL : the type of http method
     @return HTTPResponse if query success, None otherwise
     """
     if isinstance(params, str):
@@ -139,32 +141,49 @@ class Livebox:
       headers['Content-Type'] = content_type
 
     try:
-      self.__connection.request('POST', query, data, headers)
+      self.__connection.request(method, query, data, headers)
       response = self.__connection.getresponse()
-    except HTTPError as e:
-      print(e)
+    except NotConnected as e:
+      print('Error not connected')
+      return None
+    except InvalidURL as e:
+      print('Error InvalidURL')
+      return None
+    except BadStatusLine as e:
+      print('Error BadStatusLine')
+      return None
+    except ImproperConnectionState as e:
+      print('Error ImproperConnectionState')
+      return None
+    except HTTPException as e:
+      print('Error HTTP')
       return None
     return response
 
-  def _queryUnauth(self, query, params=''):
+  def _queryUnauth(self, query, params='', method='POST'):
     """Execute a simple query without any specific headers
 
     @param[in] query : the query string (part after / in URL)
     @param[in] params OPTIONNAL : the data (body) of the query
+    @param[in] method OPTIONNAL : the type of http method
     @return A dict which contains JSON response datas or empty str
     """
-    r_data = self.__query(query, params).read()
+    response = self.__query(query, params, method=method)
+    if not response:
+      return ""
+    r_data = response.read() 
     if r_data:
       return json.loads(r_data.decode('utf8'))
     else:
       return ""
 
-  def _queryAuth(self, query, params='', headers=dict()):
+  def _queryAuth(self, query, params='', headers=dict(), method='POST'):
     """Execute a query with authentication specific headers
 
     @param[in] query : the query string (part after / in URL)
     @param[in] params OPTIONNAL : the data (body) of the query
     @param[in] headers OPTIONNAL : the additionnal headers to put in the query
+    @param[in] method OPTIONNAL : the type of http method
     @return A dict which contains JSON response datas or empty str
     """
     headers['Cookie'] = self.__cookies
@@ -172,10 +191,11 @@ class Livebox:
     headers['X-Sah-Request-Type'] = 'idle'
     headers['X-Requested-With'] = 'XMLHttpRequest'
     headers['X-Prototype-Version'] = '1.7'
-    headers['Content-Type'] = "application/x-sah-ws-1-call+json; charset=UTF-8"
     headers['DNT'] = '1'
 
-    response = self.__query(query, params, headers)
+    response = self.__query(query, params, headers, method=method)
+    if not response:
+      return ""
     r_data = response.read()
     r_header = dict(response.getheaders())
     if r_data and r_header['Content-Type'].split(';')[0] == 'application/json':
@@ -183,34 +203,49 @@ class Livebox:
     else:
       return ""
 
-  def _sysbus(self, quest, param='{"parameters":{}}', auth=False):
+  def _sysbus(self, quest, param='{"parameters":{}}',
+              auth=False,
+              method='POST'):
     """Execute a SYSBUS call on the host
 
     @param[in] quest : the query object in string format
     @param[in] params OPTIONNAL : the data (body) of the query
     @param[in] auth OPTIONNAL : a boolean that indicated if the query must be
               executed as authenticated
+    @param[in] method OPTIONNAL : the type of http method
     """
     if auth:
-      return self._queryAuth("/sysbus/{:s}".format(quest), param)
+      return self._queryAuth("/sysbus/{:s}".format(quest), param,
+                             method=method)
     else:
-      return self._queryUnauth("/sysbus/{:s}".format(quest), param)
+      return self._queryUnauth("/sysbus/{:s}".format(quest), param,
+                               method=method)
 
   def require_auth(func):
     """ Apply this decorator to all method that require to be authentified
     This decorator put a basic verification to check that given method is call
     in a authenticated environnement
     """
-    def check_auth(self):
+    def check_auth(*args, **kwargs):
       """Check if the current connection s authenticated on the host or not
 
       If not authenticated return empty string
       """
-      if self.__is_connected:
-        return func(self)
+      if isinstance(args[0], Livebox) and args[0].__is_connected:
+        return func(*args, **kwargs)
       else:
-        return ""
+        return "AUTH NEEDED"
     return check_auth
+    
+  def action(func):
+    """ Apply this decorator to all method that cause physical action on LB
+    """
+    def check_action(*args, **kwargs):
+      """Show a message on action command
+      """
+      print('ACTION')
+      return func(*args, **kwargs)
+    return check_action
 
 
 
@@ -239,12 +274,8 @@ if __name__ == '__main__':
   lb = Livebox()
   lb.login()
 
-
   lb.logout()
-
-
-
-
-
-
-
+  
+  
+  
+  
